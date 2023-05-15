e8be454d5e5365c20ea27150e7996926c983eacf
Kontr <kontr@>
2023-05-15 22:57:44 +0600

    eqwe

---
+++ resources/views/components/leaflet-scripts.blade.php
@@ -0,0 +1,4 @@
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+<script src='https://unpkg.com/leaflet@1.8.0/dist/leaflet.js' crossorigin=''></script>
+<script src="https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.min.js"></script>
+<script src='https://unpkg.com/@turf/turf@6/turf.min.js'></script>
