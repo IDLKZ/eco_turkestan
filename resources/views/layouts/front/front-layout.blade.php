6d797cceab279968befc14450c699724c95bc0fd
Kontr <kontr@>
2023-06-01 00:02:30 +0600

    eqwe

--- resources/views/layouts/front/front-layout.blade.php
+++ resources/views/layouts/front/front-layout.blade.php
@@ -48,7 +48,7 @@
 
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-    <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+    <a href="/" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
         <h1 class="m-0"><span class="text-success">ECO</span> <span class="text-danger">Shymkent</span></h1>
     </a>
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
