bb33af4fce3c229ed06c5d88348b10148c0839bf
IDLKZ <idlkz2019@gmail.com>
2023-07-01 20:46:22 +0600

    second commit 17

--- resources/views/components/sidebar-navlink.blade.php
+++ resources/views/components/sidebar-navlink.blade.php
@@ -2,7 +2,7 @@
     <a class="nav-link" href="{{route($route)}}">
         <i class="{{$icon}}" aria-hidden="true"></i>
         <span>{{$routeName}}</span>
-        @if($count != '')
+        @if($count != '' && $count != 0)
             <span class="inline-flex items-center justify-center w-4 h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
             {{$count}}
         </span>
