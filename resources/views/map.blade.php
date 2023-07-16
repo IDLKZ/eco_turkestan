e3033d6f9bd0f5baadfb8891cbe9232675e76c18
idlkz <idlkz2019@gmail.com>
2023-07-16 11:55:35 +0600

    Commit "Rebranding"

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,4 +1,5 @@
 @extends("layouts.front.front-layout")
 @section("main")
-    <iframe src="https://test.idl.kz/" style="width: 100%;min-height: 100vh"></iframe>
+{{--    <iframe src="https://test.idl.kz/" style="width: 100%;min-height: 100vh"></iframe>--}}
+    <iframe src="http://localhost:4200/" style="width: 100%;min-height: 100vh"></iframe>
 @endsection
